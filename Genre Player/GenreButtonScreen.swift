48610e6c735ae3ac160512a6b714f9c0255e22bb
Mohammad Sajid Hasan <md.sajidhasan@yahoo.com>
2018-12-11 17:26:53 +0600

    Functional app

---
+++ Genre Player/GenreButtonScreen.swift
@@ -0,0 +1,49 @@
+//
+//  GenreButtonScreen.swift
+//  Genre Player
+//
+//  Created by Sajid Hasan on 11/12/18.
+//  Copyright © 2018 Sajid Hasan. All rights reserved.
+//
+
+import UIKit
+import MediaPlayer
+
+class GenreButtonScreen: UIViewController {
+    
+    var musicPlayer = MPMusicPlayerController.applicationMusicPlayer
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    @IBAction func genreButtonTapped(_ sender: UIButton) {
+        
+        MPMediaLibrary.requestAuthorization{ (status) in
+            if status == .authorized{
+                self.playGenre(genre: sender.currentTitle!)
+            }
+            
+        }
+    }
+    
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+        musicPlayer.stop()
+    }
+    @IBAction func nextButtonTapped(_ sender: UIButton) {
+        musicPlayer.skipToNextItem()
+    }
+    
+    func playGenre(genre: String){
+        
+        musicPlayer.stop()
+        
+        let query = MPMediaQuery()
+        let predicate = MPMediaPropertyPredicate(value: genre, forProperty: MPMediaItemPropertyGenre)
+        
+        query.addFilterPredicate(predicate)
+        musicPlayer.setQueue(with: query)
+        musicPlayer.shuffleMode = .songs
+        musicPlayer.play()
+    }
+}
